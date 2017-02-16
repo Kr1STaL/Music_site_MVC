87f49ccf56d4bb9e658fe27090df8ff545e2abd3
E.Shkelev <e.shkelev@itransition.com>
2017-02-16 13:22:35 +0300

    Add class_context

---
+++ Music_site_MVC/Music_MVC_Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Music_site_MVC.Models;
+
+namespace Music_site_MVC
+{
+    public class Music_MVC_Context : DbContext
+    {
+        public Music_MVC_Context()
+            :base("DbConnection")
+        { }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Akord> Akords { get; set; }
+    }
+}
