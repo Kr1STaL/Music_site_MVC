5930414b642b99a1abb430bae4aa88367173dc0c
E.Shkelev <e.shkelev@itransition.com>
2017-02-22 14:55:36 +0300

    Add new controller SongController and View for him

---
+++ Music_site_MVC/Controllers/SongController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Music_site_MVC.Controllers
+{
+    public class SongController : Controller
+    {
+        Music_MVC_Context db = new Music_MVC_Context();
+
+        // GET: Song
+        public ActionResult Index(int id)
+        {
+            var song_info = db.Songs.Find(id);
+            return View(song_info);
+        }
+    }
+}
