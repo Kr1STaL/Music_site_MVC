c307ade0e809381cf65cf642002945268de8aeaf
E.Shkelev <e.shkelev@itransition.com>
2017-02-27 14:39:55 +0300

    Add  sorting on main page

--- Music_site_MVC/Controllers/HomeController.cs
+++ Music_site_MVC/Controllers/HomeController.cs
@@ -13,11 +13,28 @@ namespace Music_site_MVC.Controllers
     {
         Music_MVC_Context db = new Music_MVC_Context();
 
-        public ActionResult Index(int? page)
+        public ActionResult Index(int? page, string sortOrder)
         {
-            int pageSize = 1;
+            int pageSize = 2;
             int pageNumber = (page ?? 1);
-            return View(db.Artists.ToList().ToPagedList(pageNumber, pageSize));
+            var artists = from s in db.Artists
+                           select s;
+            switch (sortOrder)
+            {
+                case "Name desc":
+                    artists = artists.OrderByDescending(s => s.Name);
+                    break;
+                case "Songs":
+                    artists = artists.OrderBy(s => s.Songs.Count);
+                    break;
+                case "Songs desc":
+                    artists = artists.OrderByDescending(s => s.Songs.Count);
+                    break;
+                default:
+                    artists = artists.OrderBy(s => s.Name);
+                    break;
+            }
+            return View(artists.ToList().ToPagedList(pageNumber, pageSize));
         }
     }
 }
